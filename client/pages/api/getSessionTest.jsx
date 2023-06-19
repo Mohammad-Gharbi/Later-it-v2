465552aa475c33bbf59571900e475f98de3b507e
Mohammad-Gharbi <91133928+Mohammad-Gharbi@users.noreply.github.com>
2023-06-19 14:59:09 +0100

    getSessionTest

---
+++ client/pages/api/getSessionTest.jsx
@@ -0,0 +1,30 @@
+import Cors from "cors"
+import { PrismaClient } from "@prisma/client"
+import { getSession } from "@auth0/nextjs-auth0"
+
+const prisma = new PrismaClient()
+
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function getSessionTest(req, res) {
+  await runMiddleware(req, res, cors)
+
+  const session = await getSession(req, res)
+  if (session) {
+    res.status(200).json(session)
+  }
+}
