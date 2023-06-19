b71a5d57c792ba92b00f558651792b077ade1829
Mohammad-Gharbi <91133928+Mohammad-Gharbi@users.noreply.github.com>
2023-06-19 17:59:56 +0100

    test

--- client/pages/api/getResopnse.jsx
+++ client/pages/api/getResopnse.jsx
@@ -1,24 +1,24 @@
-import Cors from "cors"
+// import Cors from "cors"
 import prisma from "../../prisma/prisma"
 
-const cors = Cors({
-  methods: ["POST", "GET", "HEAD"],
-})
+// const cors = Cors({
+//   methods: ["POST", "GET", "HEAD"],
+// })
 
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
+// function runMiddleware(req, res, fn) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result) => {
+//       if (result instanceof Error) {
+//         return reject(result)
+//       }
 
-      return resolve(result)
-    })
-  })
-}
+//       return resolve(result)
+//     })
+//   })
+// }
 
 export default async function getTag(req, res) {
-  await runMiddleware(req, res, cors)
+  // await runMiddleware(req, res, cors)
   // const result = await prisma.article.findMany({
   //   where: { userId: "mohammed.gharbi.dev@gmail.com" },
   // })
